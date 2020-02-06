29f936e14bcb7d87bd35d2947970b190640284b4
3294713004wlb <3294713004@qq.com>
2020-02-06 13:37:32 +0800

    更新CHANGELOG.md、一键更新.bat

--- 一键更新.bat
+++ 一键更新.bat
@@ -15,4 +15,16 @@ git prune
 @echo on
 echo Libraries_For_Mixly更新完成, Enjoy it!
 @echo off
+for /f "delims=" %%i in (address.txt) do (
+set a=%%i
+)
+echo %a%
+set b=%a%\blockly\blocks\company
+set c=%a%\blockly\generators\arduino\company
+set d=%a%\blockly\media\Arduino\
+echo %b%
+echo %c%
+xcopy %~dp0\block\Arduino.js %b%
+xcopy %~dp0\generator\Arduino.js %c%
+xcopy %~dp0\media\Arduino %d% /s
 pause
