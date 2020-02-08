3b207602a56e3d71292e26b3620bd642ee9039c4
3294713004wlb <3294713004@qq.com>
2020-02-08 22:30:50 +0800

    更新 导入Arduino库到Mixly.bat

--- 导入Arduino库到Mixly.bat
+++ 导入Arduino库到Mixly.bat
@@ -31,7 +31,7 @@ echo 更改media下文件 -- %e%
 echo ------------------------------------------------------------------------------------------------------------------------
 echo.
 echo 请确认路径是否正确！
-echo 若正确，请Enter键继续；若不正确，请退出修改C:\address.txt下路径
+echo 若正确，请按Enter键继续；若不正确，请退出修改C:\address.txt下路径
 pause
 
 @echo on
@@ -44,6 +44,38 @@ echo A|xcopy Arduino.xml %d%
 echo A|xcopy make.xml %d%
 echo A|xcopy %~dp0\media %e% /s
 @echo off
+
+echo.
+echo 导入库成功
+echo.
+
+if not exist C:\address_arduino.txt goto nofile_arduino
+
+for /f "delims=" %%m in (C:\address_arduino.txt) do (
+set n=%%m
+)
+if "%n%"=="D:\mixly\mixly_2019_09_15\Mixly\arduino" goto nofile_arduino1
+if "%n%"=="" goto nofile_arduino2
+
+echo.
+echo ------------------------------------------------------------------------------------------------------------------------
+echo arduino所在路径 -- %n%
+echo ------------------------------------------------------------------------------------------------------------------------
+echo 目标libraries所在路径 -- %n%\libraries\
+echo ------------------------------------------------------------------------------------------------------------------------
+
+echo.
+echo 请确认Arduino和libraries所在路径是否正确！
+echo 若正确，请按Enter键继续；若不正确，请退出修改C:\address_libraries.txt下路径
+pause
+
+@echo on
+echo A|xcopy %~dp0\Arduino库文件 %n%\ /s
+@echo off
+
+echo.
+echo 导入libraries成功
+echo.
 pause
 exit
 
@@ -58,6 +90,21 @@ pause
 exit
 
 :nofile2
-echo C:\下address.txt无有效的路径，请添加此计算机中Mixly的路径到此文本
+echo C:\下address.txt无有效路径，请添加此计算机中Mixly的路径到此文本
+pause
+exit
+
+:nofile_arduino
+echo C:\下无address_arduino.txt，请新建一个address.txt后再尝试执行此文件
+pause
+exit
+
+:nofile_arduino1
+echo C:\下address_arduino.txt为示例文本，请将文本中的路径改为此计算机中arduino的路径
+pause
+exit
+
+:nofile_arduino2
+echo C:\下address_arduino.txt无有效路径，请添加此计算机中arduino的路径到此文本
 pause
 exit
