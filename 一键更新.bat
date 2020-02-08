0d554caab74f57dab8a33e6294f193d5d767a1c8
3294713004wlb <3294713004@qq.com>
2020-02-08 22:58:15 +0800

    更新 Arduino库文件、一键更新.bat

--- 一键更新.bat
+++ 一键更新.bat
@@ -1,12 +1,6 @@
 @echo off
 title Libraries_For_Mixly 升级助手
 echo %~dp0
-
-@echo off
-del /f /s /q %~dp0\Arduino库文件\*.*
-rd /q /s %~dp0\Arduino库文件\
-md %~dp0\Arduino库文件
-
 @echo on
 echo Libraries_For_Mixly 正在升级中，请等待...
 @echo off
