64b303ea283983251aebeb85899cc75e89b9d523
3294713004wlb <3294713004@qq.com>
2020-03-19 13:23:19 +0800

    添加 第三方库结构生成.py

---
+++ Mixly第三方库制作教程/第三方库结构生成.py
@@ -0,0 +1,76 @@
+import os
+
+libraries_name = "lib_AS608" #用于定义存放 Arduino库文件 的目录名
+block_name = "AS608" #用于定义block&generator下文件和xml文件的文件名
+name = "AS608指纹识别传感器" #用于定义存放所有文件夹的目录名
+
+dir = os.getcwd()+ "\\Mixly库目录" #用于定义生成的文件夹要存放的位置
+print(dir)
+
+os.chdir(dir)
+os.mkdir(dir + "\\" + name)
+dir = dir + "\\" + name
+os.chdir(dir)
+os.mkdir(dir + "\\generator")
+os.mkdir(dir + "\\block")
+os.mkdir(dir + "\\language")
+os.mkdir(dir + "\\media")
+os.mkdir(dir + "\\companypin")
+os.mkdir(dir + "\\hardware")
+os.mkdir(dir + "\\" + libraries_name)
+
+os.mkdir(dir + "\\language\\" + block_name)
+os.mkdir(dir + "\\media\\" + block_name)
+os.mkdir(dir + "\\companypin\\" + block_name)
+os.mkdir(dir + "\\hardware\\arduino")
+
+xml = open(block_name + ".xml", 'w')
+xml.write("<!--\n")
+xml.write("type=\"company\"\n")
+xml.write("block=\"block/" + block_name + ".js\"\n")
+xml.write("generator=\"generator/" + block_name + ".js\" \n")
+xml.write("language=\"language/" + block_name + "/\"\n")
+xml.write("media=\"media/" + block_name + "\"\n")
+xml.write("pin=\"companypin/" + block_name + "\"\n")
+xml.write("lib=\"" + libraries_name + "\"\n")
+xml.write("hardware=\"hardware/arduino\" \n")
+xml.write("-->\n")
+xml.write("<!--\n")
+xml.write("Author:\n")
+xml.write("Date:\n")
+xml.write("E-mail:\n")
+xml.write("-->\n")
+xml.close()
+
+dir_generator = dir + "\\generator"
+dir_block = dir + "\\block"
+dir_language = dir + "\\language\\" + block_name
+dir_companypin = dir + "\\companypin\\" + block_name
+
+os.chdir(dir_generator)
+generator = open(block_name + ".js", 'w')
+generator.write("\'use strict\';\n")
+generator.write("goog.provide(\'Blockly.Arduino." + block_name + "\');\n")
+generator.write("goog.require(\'Blockly.Arduino\');\n")
+generator.close()
+
+os.chdir(dir_block)
+block = open(block_name + ".js", 'w')
+block.write("\'use strict\';\n")
+block.write("goog.provide(\'Blockly.Blocks." + block_name + "\');\n")
+block.write("goog.require(\'Blockly.Blocks\');\n")
+block.close()
+
+os.chdir(dir_language)
+language = open("zh-hans.js", 'w')
+language.close()
+language = open("zh-hant.js", 'w')
+language.close()
+language = open("spa.js", 'w')
+language.close()
+language = open("en.js", 'w')
+language.close()
+
+os.chdir(dir_companypin)
+companypin = open("pin.js", 'w')
+companypin.close()
